b844281852d053da3c9255372693023984144462
Kinne237 <kinnengoumlack@gmail.com>
2025-05-08 17:33:57 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ICAB_chat',
+  webDir: 'www'
+};
+
+export default config;
